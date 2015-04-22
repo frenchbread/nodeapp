4713547374fc194aa929232f5e4357e3e3c44796
Damir Mustafin <frenchthebread@gmail.com>
2015-04-23 00:57:40 +0300

    reinit

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-var express = require('express')
-  , app = express()
-  , port = process.env.PORT || 3000
+var express = require('express');
+var app = express();
 
-app.set('views', __dirname + '/views')
-app.engine('jade', require('jade').__express)
-app.set('view engine', 'jade')
+var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'))
-app.use(require('./controllers'))
+app.set('views', __dirname + '/views');
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
+app.use(require('./routes'));
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 app.listen(port, function() {
-  console.log('Listening on port ' + port)
-})
+  console.log('Listening on port ' + port);
+});
