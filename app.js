db5b0020b7733a5e03780f2374a1b60a8458d7e1
Damir Mustafin <frenchthebread@gmail.com>
2015-04-23 02:46:22 +0300

    socket.io messaging support

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 3000;
 
 app.set('views', __dirname + '/views');
@@ -18,4 +17,31 @@ app.use(function(req, res, next) {
 
 app.listen(port, function() {
   console.log('Listening on port ' + port);
+});*/
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var express = require('express');
+
+app.set('views', __dirname + '/views');
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
+app.use(require('./routes'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
 });
